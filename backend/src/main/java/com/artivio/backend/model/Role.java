3c44e13e6d0df52d2d34974c1ebb4b23bc4d7bab
NgoTan <ngovantannvt04@gmail.com>
2025-11-11 10:20:04 +0700

    update

--- backend/src/main/java/com/artivio/backend/model/Role.java
+++
@@ -1,6 +0,0 @@
-package com.artivio.backend.model;
-
-public enum Role {
-    USER,
-    ADMIN
-}
--- backend/src/main/java/com/artivio/backend/model/request/LoginRequest.java
+++
@@ -1,9 +0,0 @@
-package com.artivio.backend.model.request;
-
-import lombok.Data;
-
-@Data
-public class LoginRequest {
-    private String email;
-    private String password;
-}
