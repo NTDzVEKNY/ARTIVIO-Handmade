bb9561d61f03207042e4b0a662c64f97726eb30b
NTDzVEKNY <ntdgdeptrai@gmail.com>
2025-12-20 00:20:56 +0700

    feat: add axios for call api

---
+++ frontend/src/lib/axios.ts
@@ -0,0 +1,14 @@
+import axios from "axios";
+
+const BASE_URL = process.env.NEXT_PUBLIC_BACKEND_URL || "http://localhost:8080/api";
+
+export const axiosClient = axios.create({
+    baseURL: BASE_URL,
+    headers: { "Content-Type": "application/json" },
+});
+
+// Tạo instance riêng cho Auth để attach interceptors
+export const axiosAuth = axios.create({
+    baseURL: BASE_URL,
+    headers: { "Content-Type": "application/json" },
+});
